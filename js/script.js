315d61fc9d0c92e4dbbe524e4ad09fac77dc67f6
Nick Savini <nicholasmsavini@gmail.com>
2017-05-23 21:36:02 -0400

    Remove cat images from index.html
    Place cat images in script.js to be served dynamically.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-    var $body = $('body');
-    var kittens = ['Mittens', 'Whiskers'];
+    var $kittenElem = $('#kittens');
+
+    var kittens = ['Chester', 'Fluffernutter'];
+
     $.each(kittens, function(index) {
-        console.log(kittens[index]);
+        $kittenElem.append('<img src="img/' + kittens[index] +
+                           '.jpg" class="img-responsive col-md-3" id="kitten">');
     });
     return false;
 });
