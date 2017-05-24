3858529bbc63808777ff91817569f9bccf5d7331
Nick Savini <nicholasmsavini@gmail.com>
2017-05-23 21:38:22 -0400

    Move click counter to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // generate kittens
     var $kittenElem = $('#kittens');
 
     var kittens = ['Chester', 'Fluffernutter'];
@@ -6,6 +7,14 @@ $(document).ready(function () {
     $.each(kittens, function(index) {
         $kittenElem.append('<img src="img/' + kittens[index] +
                            '.jpg" class="img-responsive col-md-3" id="kitten">');
+    });
+
+
+    // increment click count
+    var counter = 0;
+    $('img').click(function() {
+        counter++;
+        $('#number').html(counter);
     });
     return false;
 });
