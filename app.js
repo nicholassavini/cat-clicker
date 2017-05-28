d5fc60107830b709666f65777be834dea2f27296
Nick Savini <nicholasmsavini@gmail.com>
2017-05-28 19:39:13 -0400

    Add basic MVO framework

---
+++ app.js
@@ -0,0 +1,23 @@
+$(function() {
+    var model = {
+        kittens = [
+            {name: 'Chester', image: 'img/Chester.jpg', clicks: 0},
+            {name: 'Fluffernutter', image: 'img/Fluffernutter.jpg', clicks: 0},
+            {name: 'Marsha', image: 'img/Marsha.jpg', clicks: 0},
+            {name: 'Scruffy', image: 'img/Scruffy.jpg', clicks: 0},
+            {name: 'Sherbert', image: 'img/Sherbert.jpg', clicks: 0}
+        ];
+    };
+
+    var octopus {
+
+    };
+
+    var listView {
+
+    };
+
+    var catView {
+
+    }
+})
